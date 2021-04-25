c7f08ffe4e5b8431619421f1bb267652638ef5a1
Serdar Yegulalp <serdar@genjipress.com>
2021-04-25 10:23:50 -0400

    update example for arrays

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from jit import jit, j_types as j
 
-arr = j.array(j.u8, [2, 32])
+arr = j.array(j.u8, [2, 80, 25])
 x = arr()
-x[0][0] = 5
-x[1][0] = 32
+
+x[0][0][0] = 5
+x[1][79][24] = 32
+
+# array is automatically passed by reference
 
 
 @jit
 def main(a: arr):
-    a[0][0] = 100
+    a[0][0][0] = 100
     return a
 
 
 y = main(x)
-print(y[0][0]) # 100 instead of 5
-print(y[1][0]) # still 32
+print(y[0][0][0])  # 100 instead of 5
+print(y[1][79][24])  # still 32
